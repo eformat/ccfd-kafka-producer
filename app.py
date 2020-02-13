d5fb3bfb68f10c764a359a67a008f5e6cd0fe606
Rui Vieira <ruidevieira@googlemail.com>
2020-02-13 11:13:55 +0000

    Add Kaggle features to payload

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
+from kafka.errors import KafkaError, NoBrokersAvailable
 import json
 import time
 import os
 import random
 import csv
 from itertools import cycle
+from time import sleep
+import logging
 
-producer = KafkaProducer(bootstrap_servers=[os.environ['BROKER_URL']])
-topic = os.environ['KAFKA_TOPIC']
+BROKER_URL = os.environ['BROKER_URL']
+KAFKA_TOPIC = os.environ['KAFKA_TOPIC']
+MAX_RETRIES = 100
 
-with open('data/data.csv') as f:
+logging.info(f"Using broker at {BROKER_URL}")
+logging.info(f"Using topic '{KAFKA_TOPIC}'")
+
+def get_producer():
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            result = KafkaProducer(bootstrap_servers=[BROKER_URL])
+            return result
+        except KafkaError:
+            logging.warning("No broker available. Retrying.")
+            retries += 1
+            sleep(2)        
+
+producer = get_producer()
+
+with open('data/creditcard-sample10k.csv') as f:
     reader = csv.reader(f)
     next(reader, None)  # skip the headers
     data = list(reader)
 
 pool = cycle(data)
 
+unique_id = 0
 for transaction in pool:
-    data = {"id" : int(transaction[0]), "amount" : float(transaction[4])}
+    data = {
+        "id" : unique_id, 
+        "amount" : float(transaction[30]),
+        "features" : [float(feature) for feature in transaction[2:30]]
+    }
+    unique_id += 1
     payload = json.dumps(data)
     print(f"Sending: {payload}")
-    producer.send(topic, payload.encode('utf-8'))
+    producer.send(KAFKA_TOPIC, payload.encode('utf-8'))
     time.sleep(1.0 + random.random()*2.0) # between 1 and 3 seconds 
