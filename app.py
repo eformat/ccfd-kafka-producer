f0ad5712e12a502531c8bc3ed214b978b6559b88
Rui Vieira <ruidevieira@googlemail.com>
2020-02-13 11:13:55 +0000

    Match payload format with ODH demo

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ BROKER_URL = os.environ['BROKER_URL']
 KAFKA_TOPIC = os.environ['KAFKA_TOPIC']
 MAX_RETRIES = 100
 
+logging.basicConfig(level=logging.INFO)
 logging.info(f"Using broker at {BROKER_URL}")
 logging.info(f"Using topic '{KAFKA_TOPIC}'")
 
@@ -29,22 +30,53 @@ def get_producer():
 
 producer = get_producer()
 
-with open('data/creditcard-sample10k.csv') as f:
-    reader = csv.reader(f)
-    next(reader, None)  # skip the headers
-    data = list(reader)
+def read_data(filename):
+    with open(filename, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        csv_string = list(reader)
+    class_one = []
+    class_zero=[]
+    for each in csv_string[1:]:
+        if each[-1] == "0":
+            class_zero.append(each)
+        else:
+            class_one.append(each)
 
-pool = cycle(data)
+    return [class_zero,class_one]
 
-unique_id = 0
-for transaction in pool:
-    data = {
-        "id" : unique_id, 
-        "amount" : float(transaction[30]),
-        "features" : [float(feature) for feature in transaction[2:30]]
-    }
-    unique_id += 1
-    payload = json.dumps(data)
-    print(f"Sending: {payload}")
-    producer.send(KAFKA_TOPIC, payload.encode('utf-8'))
-    time.sleep(1.0 + random.random()*2.0) # between 1 and 3 seconds 
+def sendMessage(payload,topic,producer):
+    producer.send(topic, payload)
+    logging.info(f"Sending {payload}")
+    producer.flush()
+
+
+messages = read_data('data/creditcard-sample10k.csv')
+
+class_zero = messages[0]
+class_one = messages[1]
+
+one_pointer=0
+zero_pointer=0
+print(len(class_zero))
+print(len(class_one))
+
+
+while True:
+    prob = random.randint(1,6)
+    if prob == 5:
+        if one_pointer < len(class_one):
+            sendMessage(json.dumps(class_one[one_pointer]).encode('utf-8'), KAFKA_TOPIC, producer)
+            one_pointer = one_pointer + 1
+        else:
+            one_pointer = 0
+            sendMessage(json.dumps(class_one[one_pointer]).encode('utf-8'), KAFKA_TOPIC, producer)
+    else:
+        if zero_pointer < len(class_zero):
+            sendMessage(json.dumps(class_zero[zero_pointer]).encode('utf-8'), KAFKA_TOPIC, producer)
+            zero_pointer = zero_pointer + 1
+        else:
+            one_pointer = 0
+            sendMessage(json.dumps(class_zero[zero_pointer]).encode('utf-8'), KAFKA_TOPIC, producer)
+
+    time.sleep(random.randint(2,6))
+    time.sleep(random.randint(2,6))
